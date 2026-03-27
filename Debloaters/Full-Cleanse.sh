a2657404c62fd4a4ccf998e71919398c6b83e116
Eric Luce <eluce11232011@gmail.com>
2026-03-27 06:58:11 -0400

    Create Full-Cleanse.sh

---
+++ Debloaters/Full-Cleanse.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+clear
+echo "This script debloats your android phone with a long list of applications, GApps are removed to save as much power as possible, standard adb is used instead of root so a factory reset can resolve any issues, or you can restore the apps if you're brave with the terminal!"
+#sleep 10
+apps=(
+com.google.android.gms
+com.android.vending
+com.google.ar.core
+com.google.android.odad
+io.chaldeaprjkt.gamespace
+com.google.android.apps.betterbug
+com.android.yadayada
+com.google.android.safetycore
+com.google.android.contactkeys
+org.lineageos.glimpse
+com.android.dialer
+io.chaldearprjkt.gamespace
+com.google.android.apps.walletnfcrel
+com.fitbit.FitbitMobile
+com.google.android.apps.adm
+com.google.ar.core
+com.google.pixel.livewallpaper
+com.google.android.apps.restore
+org.lineageos.audiofx
+org.lineageos.jelly
+com.google.android.apps.healthdata
+com.android.calculator2
+org.lineageos.etar
+com.google.android.apps.wellbeing
+com.google.android.apps.tips
+com.google.android.apps.tachyon
+com.google.android.apps.chromecast.app
+org.fossify.gallery
+com.google.android.videos
+com.google.android.apps.subscriptions.red
+com.google.android.apps.magazines
+com.google.android.apps.docs.editors.docs
+com.google.android.apps.youtube.music
+org.lineageos.twelve
+uk.akane.accord
+com.chiller3.bcr
+com.google.android.calculator
+com.google.android.apps.work.clouddpc
+com.google.android.apps.wearables.maestro.companion
+com.google.android.apps.calendar
+com.google.android.apps.recorder
+com.google.android.apps.safetyhub
+com.google.android.projection.gearhead
+com.google.android.calendar
+com.google.android.apps.emojiwallpaper
+com.google.android.apps.aiwallpapers
+com.google.android.apps.carrier.log
+com.google.android.apps.nexuslauncher.overlay.nogesturehint
+com.google.android.apps.pixel.creativeassistant
+com.google.android.apps.pixel.familyspace
+com.google.android.apps.pixel.relationships
+com.google.android.apps.pixel.support
+com.google.android.apps.wallpaper.pixel
+com.android.adservices.api
+com.android.backupconfirm
+com.google.audio.hearing.visualization.accessibility.scribe
+com.google.android.safetycore
+com.google.android.accessibility.soundamplifier
+com.google.android.apps.accessibility.voiceaccess
+com.android.companiondevicemanager
+com.android.providers.telephony.auto_generated_characteristics_rro
+com.android.providers.settings.auto_generated_rro_vendor__
+com.google.android.apps.weather
+org.fossify.gallery
+com.google.android.apps.diagnosticstool
+)
+for app in "${apps[@]}"; do
+echo "Uninstalling: $app"
+adb shell pm uninstall --user 0 "$app"
+done
+clear
+echo "Would You Like To Reboot Your Phone?"
+PS3='Please Select Your Choice: '
+options=("Reboot Now" "Reboot To Bootloader" "Reboot To Recovery" "End The Script")
+select opt in "${options[@]}"
+do
+case $opt in
+"Reboot Now")
+adb reboot
+;;
+"Reboot To Bootloader")
+adb reboot bootloader
+;;
+"Reboot To Recovery")
+adb reboot recovery
+;;
+"End The Script")
+exit
+;;
+*)
+echo "Invalid Option: $REPLY"
+;;
+esac
+done
