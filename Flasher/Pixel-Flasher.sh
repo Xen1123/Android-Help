29015d51c32861525967d0ebaa15b549a242a7e5
Xen1123 <eluce11232011@gmail.com>
2026-05-02 11:33:47 -0400

    Added rebooting for Bootloader and Radio flashing for Pixel-Flasher.sh, just like how the android flash tool handles it. https://flash.android.com

--- Flasher/Pixel-Flasher.sh
+++ Flasher/Pixel-Flasher.sh
@@ -47,9 +47,11 @@ do
             break
             ;;
         "Full Factory")
+            fastboot flash bootloader bootloader.img >/dev/null 2>&1 || { clear; echo "Bootloader image not found, cannot continue!"; exit 1; }
+            timeout 2s fastboot reboot-bootloader >/dev/null 2>&1 || { clear; echo "Failed To Reboot To Bootloader. Not Vital, But Still Not Great!"; }
+            fastboot flash radio radio.img >/dev/null 2>&1 || { clear; echo "Radio image not found, cannot continue!"; exit 1; }
+            timeout 2s fastboot reboot-bootloader >/dev/null 2>&1 || { clear; echo "Failed To Reboot To Bootloader. Not Vital, But Still Not Great!"; }
             images=(
-                bootloader
-                radio
                 boot
                 dtbo
                 vendor_boot
