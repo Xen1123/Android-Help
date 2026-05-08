e1e352e2e5d608c9c6657bc86bdd9b18e8f1c2ac
Xen1123 <eluce11232011@gmail.com>
2026-05-07 23:28:40 -0400

    Simplified flasher, just practicing Python still

--- Flasher/Android-Flasher.py
+++ Flasher/Android-Flasher.py
@@ -23,139 +23,17 @@ subprocess.run(["fastboot", "reboot", "fastboot"])
 
 subprocess.run(["fastboot", "devices"])
 time.sleep(2)
-file_path = Path("recovery.img")
-if file_path.is_file():
-    subprocess.run(["fastboot", "flash", "recovery", file_path])
-else:
-    print("Error: recovery.img not found. You Either Don't Use A 'recovery.img' Device, Or You Are Missing Files.")
 
-file_path = Path("vendor_boot.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "vendor_boot", file_path
-    ])
-else:
-    print("Error: vendor_boot.img not found. You Either Don't Use A 'vendor_boot.img' Device, Or You Are Missing Files.")
+partitions = [
+    "recovery", "vendor_boot", "boot", "dtbo", "vbmeta", "init_boot", "odm", "system", "vendor", "system_ext", "product", "system_dlkm", "vendor_dlkm", "system_ext", "vbmeta_vendor", "vbmeta_system"
+]
+for part in partitions:
+    file_path = Path(f"{part}.img")
 
-file_path = Path("boot.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "boot", file_path
-    ])
-else:
-    print("Error: boot.img not found.")
-
-file_path = Path("dtbo.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "dtbo", file_path
-    ])
-else:
-    print("Error: dtbo.img not found.")
-
-file_path = Path("vbmeta.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "vbmeta", file_path
-    ])
-else:
-    print("Error: vbmeta.img not found.")
-
-file_path = Path("init_boot.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "init_boot", file_path
-    ])
-else:
-    print("Error: init_boot.img not found.")
-
-file_path = Path("odm.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "odm", file_path
-    ])
-else:
-    print("Error: odm.img not found.")
-
-file_path = Path("system.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "system", file_path
-    ])
-else:
-    print("Error: system.img not found.")
-
-file_path = Path("vendor.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "vendor", file_path
-    ])
-else:    
-    print("Error: vendor.img not found.")
-
-file_path = Path("system_ext.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "system_ext", file_path
-    ])
-else:
-    print("Error: system_ext.img not found.")
-
-file_path = Path("product.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "product", file_path
-    ])
-else:
-    print("Error: product.img not found.")
-
-file_path = Path("system_dlkm.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "system_dlkm", file_path
-    ])
-else:
-    print("Error: system_dlkm.img not found.")
-
-file_path = Path("vendor_dlkm.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "vendor_dlkm", file_path
-    ])
-else:
-    print("Error: vendor_dlkm.img not found.")
-
-file_path = Path("system_ext.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "system_ext", file_path
-    ])
-else:
-    print("Error: system_ext.img not found.")
-
-file_path = Path("vbmeta.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "vbmeta", file_path
-    ])
-else:
-    print("Error: vbmeta.img not found.")
-
-file_path = Path("vbmeta_vendor.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "vbmeta_vendor", file_path
-    ])
-else:
-    print("Error: vbmeta_vendor.img not found.")
-
-file_path = Path("vbmeta_system.img")
-if file_path.is_file():
-    subprocess.run([
-        "fastboot", "flash", "vbmeta_system", file_path
-    ])
-else:
-    print("Error: vbmeta_system.img not found.")
+    if file_path.is_file():
+        print(f"Flashing {part}. . . ")
+        subprocess.run([
+            "fastboot", "flash", "part", "file_path"
 
 if os.name == 'nt':
     os.system('cls')
