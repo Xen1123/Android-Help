06c9057cb1325f34819e1c9647bb954eba456913
Xen1123 <eluce11232011@gmail.com>
2026-05-09 13:50:27 -0400

    Created Python (Linux, MacOS, Windows, BSD Compatible) version of Full Factory Flasher That Was Originally Just A Bash Script

---
+++ Flasher/Full-Factory-Flash.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+import shutil
+import subprocess
+import sys
+import platform
+import os
+import time
+print("Hello, This Is A Factory Image Flasher Script For Fastboot Androids!")
+print("Please Make Sure Fastboot Is Installed And All Images Are In The Working Directory!")
+fastboot_path = shutil.which("fastboot")
+if not fastboot_path:
+    print("Fastboot Not Found In Your PATH!")
+    sys.exit()
+else:
+    print(f"Fastboot Found At: {fastboot_path}")
+
+adb_path = shutil.which("adb")
+if not adb_path:
+    print("ADB Not Found In Your PATH")
+    sys.exit()
+else:
+    print(f"ADB Found At: {adb_path}")
+
+subprocess.run([
+    "adb", "reboot", "fastboot"
+], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.run([
+    "fastboot", "reboot", "fastboot"
+], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+subprocess.run([
+    "fastboot", "devices"
+])
+
+images = [
+    "boot", "abl", "xbl", "aop", "aop_config", "bluetooth", "modem", "cpucp", "cpucp_dtb", "devcfg", "init_boot", "vendor_boot", "recovery", "vbmeta", 
+    "vbmeta_vendor", "vbmeta_system", "xbl_ramdump", "xbl_config", "dsp", "dtbo", "keymaster", "imagefv", "tz", "shrm", "pvmfw", "odm", "hyp", "uefi", 
+    "uefisecapp", "qupfw", "bootloader", "radio", "bl1", "bl2", "bl31", "gsa", "ldfw", "pbl", "tzsw"
+]
+for part in images:
+    file_path = Path(f"{part}.img")
+
+    if file_path.is_file():
+        print(f"\nFlashing {part} . . .")
+        subprocess.run([
+            "fastboot", "flash", part, file_path
+        ], stdout=subprocess.DEVNULL, stderr=subprocces.DEVNULL)
+    else:
+        print(f"{part} Not Found . . .")
+
+subprocess.run([
+    "fastboot", "reboot", "fastboot"
+])
+
+logicals = [
+    "system", "product", "vendor", "vendor_dlkm", "system_dlkm", "system_ext"
+]
+for logic in logicals:
+    file_path = Path(f"{logic}.img")
+
+    if file_path.is_file():
+        print(f"\nFlashing {logic} . . .")
+        subprocess.run([
+            "fastboot", "flash", logic, file_path
+        ], stdout=subprocess.DEVNULL, stderr=subprocces.DEVNULL)
+    else:
+        print(f"{logic} Not Found")
+
+if os.name == 'nt':
+    os.system('cls')
+else:
+    os.system('clear')
+
+subprocess.run([
+    "fastboot", "reboot", "bootloader"
+])
