1c3e07a1450c51e9856370fc0d13fe0ebea5f1e0
Xen1123 <eluce11232011@gmail.com>
2026-06-07 22:05:27 -0400

    Moved odm.img to logicals bc that's where it goes. I also removed time.sleep(2) before rebooting to fastbootd, as some devices just decide to drop the connection in that time, and rebooting just fails...

--- Flasher/Flasher.py
+++ Flasher/Flasher.py
@@ -63,9 +63,14 @@ def main():
 
         time.sleep(2)
 
+        if args.verbose:
+            subprocess.run(["fastboot", "--set-active=a"])
+        else:
+            subprocess.run(["fastboot", "--set-active=a"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
         images = [
             "boot", "abl", "xbl", "aop", "aop_config", "featenabler", "bluetooth", "modem", "cpucp", "cpucp_dtb", "devcfg", "init_boot", "vendor_boot", "recovery", "vbmeta", 
-            "vbmeta_vendor", "vbmeta_system", "xbl_ramdump", "xbl_config", "dsp", "dtbo", "keymaster", "imagefv", "tz", "shrm", "pvmfw", "odm", "hyp", "uefi", 
+            "vbmeta_vendor", "vbmeta_system", "xbl_ramdump", "xbl_config", "dsp", "dtbo", "keymaster", "imagefv", "tz", "shrm", "pvmfw", "hyp", "uefi", 
             "uefisecapp", "qupfw", "bootloader", "radio", "bl1", "bl2", "bl31", "gsa", "ldfw", "pbl", "tzsw", "multiimgoem"
         ]
         for part in images:
@@ -73,21 +78,27 @@ def main():
 
             if args.verbose:
                     if file_path.is_file():
-                        subprocess.run(["fastboot", "flash", "--slot=all", part, file_path])
+                        subprocess.run(["fastboot", "flash", part, file_path])
 
                     else:
                         print(f"\nFILE NOT FOUND: {part}.img")
             else:
                     if file_path.is_file():
                         print(f"\nFlashing {part}.img")
-                        subprocess.run(["fastboot", "flash", "--slot=all", part, file_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                        subprocess.run(["fastboot", "flash", part, file_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
                     else:
                         print(f"\nFILE NOT FOUND: {part}.img")
 
         time.sleep(2)
-        clear()
+
+        if args.verbose:
+            pass
+        else:
+            clear()
+
         print("\nRebooting To Fastbootd")
-        time.sleep(2)
+
+        subprocess.run(["fastboot", "devices"])
         if args.verbose:
             subprocess.run(["fastboot", "reboot", "fastboot"])
         else:
@@ -95,10 +106,13 @@ def main():
 
         time.sleep(2)
 
-        clear()
+        if args.verbose:
+            pass
+        else:
+            clear()
 
         images = [
-            "system", "product", "vendor", "vendor_dlkm", "system_ext", "system_dlkm"
+            "system", "product", "vendor", "vendor_dlkm", "system_ext", "system_dlkm", "odm"
         ]
 
         for logic in images:
@@ -106,7 +120,7 @@ def main():
             
             if args.verbose:
                 if file_path.is_file():
-                    subprocess.run(["fastboot", "flash", "--slot=all", logic, file_path])
+                    subprocess.run(["fastboot", "flash", logic, file_path])
                 else:
                     print(f"\nFILE NOT FOUND: {logic}.img")
             else:
@@ -117,7 +131,11 @@ def main():
                     print(f"\nFILE NOT FOUND: {logic}.img")
 
         time.sleep(2)
-        clear()
+
+        if args.verbose:
+            pass
+        else:
+            clear()
 
         confirm = input("Reboot? (You may be prompted by the android system to factory reset first, that is normal) (y/n) ")
         if confirm.lower() != 'y':
@@ -150,8 +168,13 @@ def main():
 
         time.sleep(2)
 
+        if args.verbose:
+            subprocess.run(["fastboot", "--set-active=a"])
+        else:
+            subprocess.run(["fastboot", "--set-active=a"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
         images = [
-            "boot", "dtbo", "vendor_boot", "recovery", "init_boot", "odm", "vbmeta", "vbmeta_vendor", "vbmeta_system"
+            "boot", "dtbo", "vendor_boot", "recovery", "init_boot", "vbmeta", "vbmeta_vendor", "vbmeta_system"
         ]
 
         for part in images:
@@ -159,18 +182,23 @@ def main():
             
             if args.verbose:
                 if file_path.is_file():
-                    subprocess.run(["fastboot", "flash", "--slot=all", part, file_path])
+                    subprocess.run(["fastboot", "flash", part, file_path])
                 else:
                     print(f"\nFAILED TO FLASH: {part}.img")
             else:
                 if file_path.is_file():
                     print(f"\nFlashing {part}.img")
-                    subprocess.run(["fastboot", "flash", "--slot=all", part, file_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                    subprocess.run(["fastboot", "flash", part, file_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
                 else:
                     print(f"\nFAILED TO FLASH: {part}.img")
 
         time.sleep(2)
-        clear()
+
+        if args.verbose:
+            pass
+        else:
+            clear()
+
         print("\nRebooting To Fastbootd")
         time.sleep(2)
 
@@ -181,10 +209,13 @@ def main():
 
         time.sleep(2)
 
-        clear()
+        if args.verbose:
+            pass
+        else:
+            clear()
 
         images = [
-            "system", "product", "vendor", "vendor_dlkm", "system_ext", "system_dlkm"
+            "system", "product", "vendor", "vendor_dlkm", "system_ext", "system_dlkm", "odm"
         ]
 
         for logic in images:
@@ -192,18 +223,22 @@ def main():
             
             if args.verbose:
                 if file_path.is_file():
-                    subprocess.run(["fastboot", "flash", "--slot=all", logic, file_path])
+                    subprocess.run(["fastboot", "flash", logic, file_path])
                 else:
                     print(f"\nFILE NOT FOUND: {logic}.img")
             else:
                 if file_path.is_file():
                     print(f"\nFlashing {logic}.img")
-                    subprocess.run(["fastboot", "flash", "--slot=all", logic, file_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                    subprocess.run(["fastboot", "flash", logic, file_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
                 else:
                     print(f"\nFILE NOT FOUND: {logic}.img")
 
         time.sleep(2)
-        clear()
+
+        if args.verbose:
+            pass
+        else:
+            clear()
 
         confirm = input("Reboot? (You may be prompted by the android system to factory reset first, that is normal) (y/n) ")
         if confirm.lower() != 'y':
