5873b0e227ce6e06f7885d0b2cba1ece1d8dd551
Xen1123 <eluce11232011@gmail.com>
2026-07-07 18:02:03 -0400

    Started master.py, a combination of Debloat.py and Flasher.py

---
+++ master.py
@@ -0,0 +1,281 @@
+import os, subprocess, shutil, sys, time, argparse, urllib
+from pathlib import Path
+
+apps = [
+    "com.google.android.apps.bard",
+    "com.samsung.sree",
+    "com.samsung.kidsplay",
+    "com.samsung.android.kidsinstaller",
+    "com.samsung.android.homemade",
+    "com.samsung.aasaservice",
+    "com.samsung.android.app.updatecenter",
+    "com.samsung.android.mapsagent",
+    "com.samsung.android.bbc.bbcagent",
+    "android.autoinstalls.config.samsung",
+    "com.samsung.android.app.omcagent",
+    "com.samsung.android.livestickers",
+    "com.samsung.android.rubin.app",
+    "com.samsung.android.sdm.config",
+    "com.samsung.android.game.gametools",
+    "com.samsung.android.game.gamehome",
+    "com.samsung.android.game.gos",
+    "com.samsung.gpuwatchapp",
+    "com.samsung.android.app.spage",
+    "com.sec.spp.push",
+    "com.samsung.android.stickercenter",
+    "com.sec.android.app.ve.vebgm",
+    "com.sec.android.widgetapp.samsungapps",
+    "com.audible.application",
+    "com.google.ar.core",
+    "com.sec.android.preloadinstaller",
+    "com.microsoft.skydrive",
+    "com.sec.android.daemonapp",
+    "com.google.android.music",
+    "com.sec.android.app.popupcalculator",
+    "com.microsoft.office.officehubrow",
+    "com.samsung.android.galaxy",
+    "com.sec.android.app.sbrowser",
+    "com.samsung.android.email.provider",
+    "android.overlay.gms.region.all",
+    "android.overlay.gms.region.row",
+    "com.android.egg",
+    "com.android.microdroid.empty_payload",
+    "com.android.providers.partnerbooks",
+    "com.android.settings.overlay.personalsafety",
+    "com.android.settings.overlay.turbo",
+    "com.android.setupwizard.overlay",
+    "com.android.systemui.overlay.gms",
+    "com.aura.oobe.solutions",
+    "com.goodfix.fingerprint.setting",
+    "com.google.ambient.streaming",
+    "com.google.android.accessibility.switchaccess",
+    "com.google.android.adservices.api",
+    "com.google.android.apps.docs",
+    "com.google.android.apps.restore",
+    "com.google.android.apps.tachyon",
+    "com.google.android.apps.youtube.music",
+    "com.google.android.federatedcompute",
+    "com.google.android.feedback",
+    "com.google.android.gms.location.history",
+    "com.google.android.keep",
+    "com.google.android.ondevicepersonalization.services",
+    "com.google.android.onetimeinitializer",
+    "com.google.android.overlay.gmsconfig.healthconnect",
+    "com.google.android.overlay.gmsconfig.tetheringentitlement",
+    "com.google.android.videos",
+    "com.google.mainline.adservices",
+    "com.nothing.agreement",
+    "com.nt.android.overlay.gmsconfig.safetycenter",
+    "com.nt.android.overlay.gmsconfig.settings",
+    "com.nt.android.overlay.gmsconfig.settingsprovider",
+    "com.nt.diagswitch",
+    "com.qti.dcf",
+    "com.qti.qcc",
+    "com.qualcomm.location",
+    "com.qualcomm.qti.biometrics.fingerprint.service",
+    "com.qualcomm.qti.qms.serivce.connectionsecurity",
+    "com.google.android.odad",
+    "com.microsoft.appmanager",
+    "com.microsoft.skydrive",
+    "com.samsung.android.beaconmanager",
+    "com.samsung.android.app.omcagent",
+    "com.samsung.android.app.reminder",
+    "com.sec.android.app.billing",
+    "com.samsung.android.app.spage",
+    "com.sec.android.app.sbrowser",
+    "com.samsung.android.samsungpass",
+    "com.google.android.game.gamehome",
+    "com.samsung.android.game.gametools",
+    "com.samsung.android.aremoji",
+    "com.samsung.android.samsungpassautofill",
+    "com.sec.android.mimage.avatarstickers",
+    "io.chaldeaprjkt.gamespace",
+    "com.google.android.apps.betterbug",
+    "com.android.yadayada",
+    "com.google.android.safetycore",
+    "com.google.android.contactkeys",
+    "org.lineageos.glimpse",
+    "com.google.android.apps.walletnfcrel",
+    "com.fitbit.FitbitMobile",
+    "com.google.android.apps.adm",
+    "com.google.ar.core",
+    "com.google.pixel.livewallpaper",
+    "org.lineageos.audiofx",
+    "org.lineageos.jelly",
+    "com.google.android.apps.healthdata",
+    "com.android.calculator2",
+    "org.lineageos.etar",
+    "com.google.android.apps.tips",
+    "com.google.android.apps.chromecast.app",
+    "org.fossify.gallery",
+    "com.google.android.apps.subscriptions.red",
+    "com.google.android.apps.magazines",
+    "com.google.android.apps.docs.editors.docs",
+    "org.lineageos.twelve",
+    "uk.akane.accord",
+    "com.google.android.apps.work.cloudpc",
+    "com.google.android.apps.wearables.maestro.companion",
+    "com.google.android.apps.recorder",
+    "org.lineageos.recorder",
+    "com.google.android.apps.safetyhub",
+    "com.google.android.marvin.talkback",
+    "com.google.android.apps.emojiwallpaper",
+    "com.google.android.apps.aiwallpapers",
+    "com.google.android.apps.carrier.log",
+    "com.google.android.apps.pixel.familyspace",
+    "com.google.android.apps.pixel.creativeassistant",
+    "com.google.android.apps.pixel.relationships",
+    "com.google.android.apps.pixel.support",
+    "com.google.android.apps.wallpaper.pixel",
+    "com.android.adservices.api",
+    "com.android.backupconfirm",
+    "com.google.audio.hearing.visualization.accessibility.scribe",
+    "com.google.android.accessibility.soundamplifier",
+    "com.google.android.apps.accessibility.voiceaccess",
+    "com.android.companiondevicemanager",
+    "com.android.providers.telephony.auto_generated_characteristics_rro",
+    "com.android.providers.settings.auto_generated_rro_vendor__",
+    "com.google.android.apps.weather",
+    "com.google.android.apps.diagnosticstool",
+    "com.google.android.apps.pixel.nowplaying",
+]
+
+gstandard = [
+    "com.google.android.apps.photos",
+    "com.google.android.apps.nbu.files",
+    "com.google.android.calendar",
+    "com.google.android.gm",
+    "com.google.android.apps.maps",
+    "com.google.android.apps.searchlite",
+    "com.google.android.apps.mapslite",
+    "com.android.chrome",
+    "com.chrome.beta",
+    "com.chrome.canary",
+    "com.chrome.dev",
+    "com.google.android.youtube",
+    "com.google.android.googlequicksearchbox",
+]
+
+gFULL = [
+    "com.android.vending",
+    "com.google.android.gms",
+    "com.google.android.apps.messaging",
+    "com.google.android.contacts",
+    "com.google.android.dialer",
+    "com.google.android.googlequicksearchbox",
+    "com.google.android.apps.photos",
+    "com.google.android.apps.nbu.files",
+    "com.google.android.calendar",
+    "com.google.android.gm",
+    "com.google.android.apps.maps",
+    "com.google.android.apps.searchlite",
+    "com.google.android.apps.mapslite",
+    "com.android.chrome",
+    "com.chrome.canary",
+    "com.chrome.dev",
+    "com.google.android.youtube",
+]
+
+result = subprocess.run(["adb", "shell", "pm", "list", "packages"], capture_output=True, text=True)
+installed_apps = [line.replace("package:", "").strip() for line in result.stdout.splitlines()]
+
+clear()
+
+def verbose_clear():
+    if args.verbose:
+        pass
+    else:
+        clear()
+
+
+print("""
+ █████╗ ███╗   ██╗██████╗ ██████╗  ██████╗ ██╗██████╗ 
+██╔══██╗████╗  ██║██╔══██╗██╔══██╗██╔═══██╗██║██╔══██╗
+███████║██╔██╗ ██║██║  ██║██████╔╝██║   ██║██║██║  ██║
+██╔══██║██║╚██╗██║██║  ██║██╔══██╗██║   ██║██║██║  ██║
+██║  ██║██║ ╚████║██████╔╝██║  ██║╚██████╔╝██║██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝╚═════╝ 
+""")
+
+def main():
+    parser = argparse.ArgumentParser(
+            description="Multi-Device Tool For Debloating And Flashing",
+            epilog="Example: python master.py --debloat --noroot --verbose",
+    )
+    
+    parser.add_argument("--verbose", action="store_true", help="Shows ALL Logs And Doesn't Clear The Terminal")
+    parser.add_argument("--debloat", action="store_true", help="Debloat An Android Device With ADB")
+    parser.add_argument("--flash", action="sStatore_true", help="Flash Firmware (or ROMs) To An Android Device With Fastboot"
+    parser.add_argument("--root", action="store_true", help="Debloat An Android Device With Root Permissions, Deleting The App")
+    parser.add_argument("--noroot", action="store_true", help="Debloat An Android Device Without Root Access, Disables Apps")
+    parser.add_argument("--full", action="store_true", help="Flashes ALL Firmware To An Android Device, Includes Bootloader Images")
+    parser.add_argument("--logical", action="store_true", help="Flashes JUST Android Partitions To A Phone, Skipping Risky Bootloader Images")
+
+    args = parser.parse_args()
+    
+    if not args.debloat and not args.flash:
+        parser.print_help()
+        input("\nClick Any Key To Exit")
+        sys.exit(0)
+
+    if args.debloat:
+        print("\nDebloating Device!")
+        result = subprocess.run(["adb", "devices"], capture_output=True, text=True, check=True)
+        if "device" not in result.stdout.split():
+            input("\nNo Device!")
+            sys.exit(0)
+        if "unauthorized" in result.stdout:
+            input("\nDevice Not Authorized!")
+            sys.exit(0)
+        if not "device" in result.stdout:
+            input("\nADB Unable To Run!")
+            sys.exit(0)
+        if device in result.stdout:
+            pass
+
+        if args.root:
+            root_result = subprocess.run(["adb", "shell", "su", "-c", "whoami"], capture_output=True, text=True, check=True)
+            if "root" in root_result.stdout:
+                verbose_clear()
+                for app in apps:
+                    if app in installed_apps:
+                        if args.verbose:
+                            subprocess.run(["adb", "shell", "su", "-c", "pm", "uninstall", "--user", "0", app])
+                        else:
+                            print(f"Uninstalling: {app}")
+                            subprocess.run(["adb", "shell", "su", "-c", "pm", "uninstall", "--user", "0", app], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+                    else:
+                        pass
+
+                confirm = input("Remove The Standard Google Apps? (Photos, Chrome, Google, Gmail, Maps, keeps Play Store and Play Services)  (y/n ")
+                if confirm.lower() != "y":
+                    verbose_clear()
+                    pass
+                if confirm.lower() = "n":
+                    verbose_clear()
+                    for app in gstandard:
+                        if app in installed_apps:
+                            if args.verbose:
+                                subprocess.run(["adb", "shell", "su", "-c", "pm", "uninstall", "--user", "0", app])
+                            else:
+                                print(f"\nUninstalling: {app}")
+                                subprocess.run(["adb", "shell", "su", "-c", "pm", "uninstall", "--user", "0", app], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+
+                confirm = input("Remove The Important Google Apps? (Play Services and Play Store + apps that depend on them) (y/n) ")
+                if confirm.lower() != "y":
+                    verbose_clear()
+                    pass
+                if confirm.lower() = "n":
+                    verbose_clear()
+                    for app in gFULL:
+                        if app in installed_apps:
+                            if args.verbose:
+                                subprocess.run(["adb", "shell", "su", "-c", "pm", "uninstall", "--user", "0", app])
+                            else:
+                                print(f"\nUninstalling: {app}")
+                                subprocess.run(["adb", "shell", "su", "-c", "pm", "uninstall", "--user", "0", app], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+            elif not "root" in root_result.stdout:
+                input("Root Not Detected! If You Are Rooted, Please Make Sure You Granted `shell` Root Permissions!")
+                sys.exit(0)
+            
+
